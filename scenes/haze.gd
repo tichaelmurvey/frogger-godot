b673955e8551dc34c7f0b93cb54efe98b420010c
SteamOS User <michaelturvey97@gmail.com>
2026-02-02 14:34:39 -0500

    vignette on death

--- scenes/haze.gd
+++ scenes/haze.gd
@@ -3,8 +3,8 @@ extends Panel
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	GameEnv.death_start.connect(show)
+	GameEnv.reset_game.connect(hide)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
